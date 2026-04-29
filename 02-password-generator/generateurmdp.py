6e338951913861410cff7ef49b54354f92fb276d
Mehdi <mehdibenabdel952@gmail.com>
2026-04-30 00:54:18 +0200

    refactor: rename variables to English and add comments

--- 02-password-generator/generateurmdp.py
+++
@@ -1,9 +0,0 @@
-import random, string
-
-long = int(input("combien de caractere vous voulez pour votre mot de passe: "))
-characters = string.ascii_letters + string.digits + string.punctuation
-password = ""
-
-for i in range(long):
-  password += random.choice(characters)
-print(password)
