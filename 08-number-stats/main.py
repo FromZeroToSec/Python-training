f7697c15f71afdb64a30ab8e286113bdfce4a27c
Mehdi <mehdibenabdel952@gmail.com>
2026-05-21 14:43:01 +0200

    feat(08-number-stats): add stats calculation function

--- 08-number-stats/main.py
+++ 08-number-stats/main.py
@@ -11,5 +11,22 @@ def get_number():
             print("Invalid input. Please enter a valid number.")
     return numbers
         
+
+def calculate_stats(numbers):
+    smallest = min(numbers)
+    biggest = max(numbers)
+    addition =sum(numbers)
+    average = sum(numbers) / len(numbers)
+    return smallest, bigest, average, addition
+
+
+
+
 if __name__ == "__main__":
     get_number()        
+
+
+
+
+
+    # fonctions built-in
