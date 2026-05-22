8467cbb3dfed44fc71e8c2fb084cb344a8f2d0a4
Mehdi <mehdibenabdel952@gmail.com>
2026-05-22 06:08:49 +0200

    feat(08-number-stats): add display separator and finalize output

--- 08-number-stats/main.py
+++ 08-number-stats/main.py
@@ -1,7 +1,7 @@
 def get_number():
     numbers = []
     while True :
-        user_input = input("Enter 'Q' to quit: \nEnter a number: ").upper()
+        user_input = input("Enter 'Q' to quit or Enter a number: ").upper()
         if user_input == "Q": 
                 break
         try:    
@@ -17,16 +17,18 @@ def calculate_stats(numbers):
     biggest = max(numbers)
     addition =sum(numbers)
     average = sum(numbers) / len(numbers)
-    return smallest, bigest, average, addition
+    return smallest, biggest, average, addition
 
 
+def main():
+    number = get_number()
+    smallest, biggest, average, addition = calculate_stats(number)
+    print(50 * "-")
+    print(f"Smallest number: {smallest}")
+    print(f"Biggest number: {biggest}")
+    print(f"Sum: {addition}")
+    print(f"Average: {average}")
 
 
 if __name__ == "__main__":
-    get_number()        
-
-
-
-
-
-    # fonctions built-in
+    main()
