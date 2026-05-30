ab70b8fbb55e06b76e8d18fa26f44806c2391c07
Mehdi <mehdibenabdel952@gmail.com>
2026-05-30 10:56:23 +0200

    feat: add loop to run fizzbuzz over a range

--- 10-fizzbuzz-extended/main.py
+++ 10-fizzbuzz-extended/main.py
@@ -1,3 +1,13 @@
 def fizzbuzz(number):
-    if number %5 == 0  and number %3 == 0:
+    if number % 5 == 0  and number % 3 == 0:
         return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return str(number)
+    
+def run_fizzbuzz(limit):
+    for number in range(1, limit + 1):
+        print(fizzbuzz(number))
