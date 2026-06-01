7d2f9cf14060ff2263295b23cdff86dc8c52ffe3
Mehdi <mehdibenabdel952@gmail.com>
2026-06-01 09:41:50 +0200

    refactor: add docstrings and fix PEP8 spacing

--- 10-fizzbuzz-extended/main.py
+++ 10-fizzbuzz-extended/main.py
@@ -1,4 +1,11 @@
 def fizzbuzz(number):
+    """
+    Returns the string representation of the given number:
+    - "Fizz" if the number is divisible by 3 but not by 5
+    - "Buzz" if the number is divisible by 5 but not by 3
+    - "FizzBuzz" if the number is divisible by both 3 and 5
+    - The number as a string otherwise
+    """
     if number % 5 == 0  and number % 3 == 0:
         return "FizzBuzz"
     elif number % 3 == 0:
@@ -8,11 +15,15 @@ def fizzbuzz(number):
     else:
         return str(number)
     
+
 def run_fizzbuzz(limit):
+    """Prints the FizzBuzz sequence up to the given limit."""
     for number in range(1, limit + 1):
         print(fizzbuzz(number))
 
+
 def main():
+    """Main function to run the FizzBuzz program."""
     try:
         limit = int(input("Enter the limit: "))
         run_fizzbuzz(limit)
