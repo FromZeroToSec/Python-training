f1638d33f0e0fe2d832667cd10728e6ba4fa2caf
Mehdi <mehdibenabdel952@gmail.com>
2026-06-10 11:20:21 +0200

    feat: add docstrings to all functions

--- 12-anagram-detector/main.py
+++ 12-anagram-detector/main.py
@@ -1,12 +1,15 @@
 def normalize(word):
+    """Removes spaces and converts to lowercase"""
     return word.lower().replace(" ", "")
 
 
 def is_anagram(word1, word2):
+    """Returns True if the given words are anagrams, False otherwise"""
     return sorted(normalize(word1)) == sorted(normalize(word2))
 
 
 def main():
+    """takes two words as input and checks if they are anagrams"""
     word1 = input("Enter the first word: ")
     word2 = input("Enter the second word: ")
     if is_anagram(word1, word2):
