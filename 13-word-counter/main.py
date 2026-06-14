c29e1b95eb7b616227242a531bbb77b7d9c3137a
Mehdi <mehdibenabdel952@gmail.com>
2026-06-14 17:59:49 +0200

    feat: add main function and __main__ guard

--- 13-word-counter/main.py
+++ 13-word-counter/main.py
@@ -6,6 +6,7 @@ def get_text():
         else:
             return text 
         
+        
 def count_words(text):
     word_count = {}
     for word in text.lower().split():
@@ -15,10 +16,22 @@ def count_words(text):
             word_count[word] = 1
     return word_count
 
+
 def get_top_words(word_count, n):
     sorted_word_count = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
     return sorted_word_count[:n] 
 
+
 def display_results(top_words):
     for word, count in top_words:
         print(f"{word}: {count}")
+
+
+def main():
+    text = get_text()
+    word_count = count_words(text)
+    top_words = get_top_words(word_count, 5)
+    display_results(top_words)
+
+if __name__ == "__main__":
+    main()
